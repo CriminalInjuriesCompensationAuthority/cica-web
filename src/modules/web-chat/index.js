357cf25d366b3c33604d973dbc16075115dcb1e2
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2020-07-22 16:03:37 +0100

    WIP: webpack configs

--- src/modules/web-chat/index.js
+++ src/modules/web-chat/index.js
@@ -1,9 +1,7 @@
 function createWebChat(element) {
     const chatIframe = element;
-    console.log({chatIframe});
     if (chatIframe) {
         const queryStringParameters = window.location.search;
-        console.log({chatIframe, queryStringParameters});
         chatIframe.src += queryStringParameters;
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,29 @@ module.exports = {
     entry: {
         'bundle.js': [
             // bundles from the bottom up.
+            path.resolve(__dirname, 'node_modules/js-cookie/src/js.cookie.js'),
+            path.resolve(__dirname, 'node_modules/axios/dist/axios.min.js'),
+            // path.resolve(__dirname, 'node_modules/time-convert/index.js'),
             path.resolve(__dirname, 'components/cica/modal/modal.js'),
             path.resolve(__dirname, 'src/js/scripts.js')
         ]
     },
     output: {
         filename: '[name]',
-        path: path.resolve(__dirname, 'public/dist/js')
+        path: path.resolve(__dirname, 'public/dist/js'),
+        globalObject: 'window'
     },
     devtool: 'none',
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
+                // exclude: /node_modules/,
+                include: [
+                    path.resolve(__dirname, 'node_modules/js-cookie/'),
+                    // path.resolve(__dirname, 'node_modules/axios/'),
+                    path.resolve(__dirname, 'node_modules/time-convert/')
+                ],
                 use: [
                     {
                         loader: 'babel-loader',
@@ -29,10 +38,10 @@ module.exports = {
                                     '@babel/preset-env',
                                     {
                                         useBuiltIns: 'usage',
-                                        corejs: 3,
+                                        corejs: 3
                                         // https://github.com/browserslist/browserslist
-                                        targets:
-                                            'last 1 version, ie >= 11, Safari >= 12, ios_saf >= 10, iOS >= 10'
+                                        // targets:
+                                        //     'last 6 versions, ie >= 9, Safari >= 12, ios_saf >= 10, iOS >= 10'
                                     }
                                 ]
                             ]
